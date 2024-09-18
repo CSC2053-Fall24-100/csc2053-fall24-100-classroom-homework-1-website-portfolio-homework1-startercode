f547751358a8f44972c131869bdcac44abdff75e
wkleman <wkleman@villanova.edu>
2024-09-17 21:19:04 -0400

    Add files via upload

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -2,17 +2,20 @@ function loadNavbar() {
     const navbar = `
         <nav class="navbar navbar-expand-xl navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.html">Home</a>
+                <a class="navbar-brand" href="../views/index.html">Home</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic" aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarBasic">
                     <ul class="navbar-nav me-auto mb-2 mb-xl-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="schedule.html">Schedule</a>
+                    <li class="nav-item">
+                            <a class="nav-link" href="../views/index.html">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="hobbies.html">Hobbies</a>
+                            <a class="nav-link" href="../views/schedule.html">Schedule</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../views/hobbies.html">Hobbies</a>
                         </li>
                     </ul>
                 </div>
